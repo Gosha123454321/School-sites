1cae3b99b6ba2d345ece93a7fef75d74227f1bde
kirillvelyugo <kirillvelyugo97@gmail.com>
2021-04-08 17:11:48 +0500

    sdf

--- School/Site v.2/script/index.js
+++ School/Site v.2/script/index.js
@@ -1,7 +1,93 @@
 document.addEventListener('DOMContentLoaded', function () {
+	
+	document.getElementById('firstType__minLenght').addEventListener('change', event => {
+		console.log(event.target.value)
+	})
+
 	let introButton = document.getElementById('introButton')
 	introButton.addEventListener('click', function (event) {
 		event.preventDefault()
 
 	})
+
+		
+	//Глобальный объект данных
+	//========================
+	let data = {
+		firstType: {
+			minLength: 0,
+			maxLength: 0,
+			minCountLetters: 0,
+			maxCountLetters: 0,
+			minPlaceWord: 0,
+			maxPlaceWord: 0,
+			countTasks: 0
+		},
+		secondType: {
+			minLength: 0,
+			maxLength: 0,
+			minCountLetters: 0,
+			maxCountLetters: 0,
+			countTasks: 0
+		},
+		thirdType: {
+			minLength: 0,
+			maxLength: 0,
+			minCountLetters: 0,
+			maxCountLetters: 0,
+			countTasks: 0
+		}
+	}
+
+	// Ввод значений для задач первого типа
+	//=====================================
+	let firstType_nextTask = document.getElementById('firstType_nextTask')
+	firstType_nextTask.addEventListener('click', function(event){
+		event.preventDefault()
+
+		data.firstType.minLength = document.getElementById('firstType__minLenght').value
+		data.firstType.maxLenght = document.getElementById('firstType__maxLenght').value
+
+		data.firstType.minCountLetters = document.getElementById('firstType__minCountLetters').value
+		data.firstType.maxCountLetters = document.getElementById('firstType__maxCountLetters').value
+
+		data.firstType.minPlaceWord = document.getElementById('firstType__minPlaceWord').value
+		data.firstType.maxPlaceWord = document.getElementById('firstType__maxPlaceWord').value
+
+		data.firstType.countTasks = document.getElementById('firstType__countTasks').value	
+	})
+
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
